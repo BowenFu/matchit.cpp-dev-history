737314093ce3e928d7f5bf2e43c65fb8be7a50a7
Bowen Fu <bowen.full@gmail.com>
2021-05-10 23:55:32 +0800

    Id

---
+++ 8-Identifier/main.cpp
@@ -0,0 +1,54 @@
+#include "core.h"
+#include "patterns.h"
+
+bool func1(int32_t v)
+{
+    return true;
+}
+
+int64_t func2(int32_t v)
+{
+    return 12;
+}
+
+template <typename V, typename U>
+void testMatch(V const &input, U const &expected)
+{
+    Id<int> ii;
+    auto x = match(input)(
+        pattern(1) = func1,
+        pattern(2) = func2,
+        pattern(or_(56, 59)) = func2,
+        // pattern(when([](auto&& x){return x < 0; })) = [](int32_t){ return -1; },
+        pattern(_ < 0) = [](int32_t){ return -1; },
+        pattern(_ < 10) = [](int32_t){ return -10; },
+        pattern(and_(_ < 17, _ > 15)) = [](int32_t){ return 16; },
+        pattern(app([](int32_t x){return x*x; }, 100)) = [](int32_t){ return 100; },
+        pattern(app([](int32_t x){return x*x; }, when([](auto&& x){return x > 1000; }))) = [](int32_t){ return 1000; },
+        pattern(ii) = [&ii](int32_t){ return ii.value() + 1; },
+        pattern(_) = [](int32_t){ return 111; }
+    );
+    static_assert(std::is_same_v<int64_t, decltype(x)>);
+    if (x == expected)
+    {
+        printf("Passed!\n");
+    }
+    else
+    {
+        printf("Failed! result: %lld, expected: %d\n", x, expected);
+    }
+}
+
+int32_t main()
+{
+    testMatch(1, true);
+    testMatch(2, 12);
+    testMatch(11, 12); // Id matched.
+    testMatch(59, 12); // or_ matched.
+    testMatch(-5, -1); // when matched.
+    testMatch(10, 100); // app matched.
+    testMatch(100, 1000); // app > when matched.
+    testMatch(5, -10); // _ < 10 matched.
+    testMatch(16, 16); // and_ matched.
+    return 0;
+}
---
+++ 8-Identifier/patterns.h
@@ -0,0 +1,294 @@
+#ifndef _PATTERNS_H_
+#define _PATTERNS_H_
+
+template <typename Pattern>
+class PatternTraits;
+
+template <typename Pattern, typename Value>
+bool match(Pattern const& pattern, Value const& value)
+{
+    return PatternTraits<Pattern>::match(pattern, value);
+}
+
+template <typename Pattern, typename Func>
+class PatternPair
+{
+public:
+    template <typename Value>
+    using RetType = std::invoke_result_t<Func, Value>;
+
+    PatternPair(Pattern const& pattern, Func const& func)
+        : mPattern{pattern}
+        , mHandler{func}
+    {
+    }
+    template <typename Value>
+    bool match(Value const& value) const
+    {
+        return ::match(mPattern, value);
+    }
+    template <typename Value>
+    auto execute(Value const& value) const
+    {
+        return mHandler(value);
+    }
+private:
+    Pattern const& mPattern;
+    Func const& mHandler;
+};
+
+template <typename Pattern>
+class PatternHelper
+{
+public:
+    explicit PatternHelper(Pattern const& pattern)
+        : mPattern{pattern}
+        {}
+    template <typename Func>
+    auto operator=(Func const& func)
+    {
+        return PatternPair<Pattern, Func>{mPattern, func};
+    }
+private:
+    Pattern const& mPattern;
+};
+
+template <typename Pattern>
+PatternHelper<Pattern> pattern(Pattern const& p)
+{
+    return PatternHelper<Pattern>{p};
+}
+
+class WildCard{};
+constexpr WildCard _;
+
+template <typename Pattern>
+class PatternTraits
+{
+public:
+    template <typename Value>
+    static bool match(Pattern const& pattern, Value const& value)
+    {
+        return pattern == value;
+    }
+};
+
+template <>
+class PatternTraits<WildCard>
+{
+    using Pattern = WildCard;
+public:
+    template <typename Value>
+    static bool match(Pattern const&, Value const&)
+    {
+        return true;
+    }
+};
+
+
+template <typename... Patterns>
+class Or
+{
+public:
+    explicit Or(Patterns const&... patterns)
+        : mPatterns{patterns...}
+        {}
+    auto const& patterns() const
+    {
+        return mPatterns;
+    }
+private:
+    std::tuple<Patterns const&...> mPatterns;
+};
+
+template <typename... Patterns>
+auto or_(Patterns const&... patterns) -> Or<Patterns...>
+{
+    return Or<Patterns...>{patterns...};
+}
+
+template <typename... Patterns>
+class PatternTraits<Or<Patterns...>>
+{
+public:
+    template <typename Value>
+    static bool match(Or<Patterns...> const& orPat, Value const& value)
+    {
+        return std::apply(
+            [&value](Patterns const&... patterns)
+            {
+                return (::match(patterns, value) || ...);
+            },
+            orPat.patterns());
+    }
+};
+
+template <typename Pred>
+class When
+{
+public:
+    explicit When(Pred const& pred)
+        : mPred{pred}
+        {}
+    auto const& predicate() const
+    {
+        return mPred;
+    }
+private:
+    Pred const mPred;
+};
+
+template <typename Pred>
+auto when(Pred const& pred)
+{
+    return When<Pred>{pred};
+}
+
+template <typename Pred>
+class PatternTraits<When<Pred>>
+{
+public:
+    template <typename Value>
+    static bool match(When<Pred> const& whenPat, Value const& value)
+    {
+        return whenPat.predicate()(value);
+    }
+};
+
+template <typename Unary, typename Pattern>
+class App
+{
+public:
+    App(Unary const& unary, Pattern const& pattern)
+        : mUnary{unary}
+        , mPattern{pattern}
+        {}
+    auto const& unary() const
+    {
+        return mUnary;
+    }
+    auto const& pattern() const
+    {
+        return mPattern;
+    }
+private:
+    Unary const& mUnary;
+    Pattern const& mPattern;
+};
+
+template <typename Unary, typename Pattern>
+auto app(Unary const& unary, Pattern const& pattern)
+{
+    return App<Unary, Pattern>{unary, pattern};
+}
+
+template <typename Unary, typename Pattern>
+class PatternTraits<App<Unary, Pattern>>
+{
+public:
+    template <typename Value>
+    static bool match(App<Unary, Pattern> const& appPat, Value const& value)
+    {
+        return ::match(appPat.pattern(), appPat.unary()(value));
+    }
+};
+
+template <typename T>
+auto operator<(WildCard const&, T const& t)
+{
+    return when([t](auto&& p){ return p < t;});
+}
+
+template <typename T>
+auto operator<=(WildCard const&, T const& t)
+{
+    return when([t](auto&& p){return p <= t;});
+}
+
+template <typename T>
+auto operator>=(WildCard const&, T const& t)
+{
+    return when([t](auto&& p){return p >= t;});
+}
+
+template <typename T>
+auto operator>(WildCard const&, T const& t)
+{
+    return when([t](auto&& p){return p > t;});
+}
+
+template <typename... Patterns>
+class And
+{
+public:
+    explicit And(Patterns const&... patterns)
+        : mPatterns{patterns...}
+        {}
+    auto const& patterns() const
+    {
+        return mPatterns;
+    }
+private:
+    std::tuple<Patterns const&...> mPatterns;
+};
+
+template <typename... Patterns>
+auto and_(Patterns const&... patterns)
+{
+    return And<Patterns...>{patterns...};
+}
+
+template <typename... Patterns>
+class PatternTraits<And<Patterns...>>
+{
+public:
+    template <typename Value>
+    static bool match(And<Patterns...> const& andPat, Value const& value)
+    {
+        return std::apply(
+            [&value](Patterns const&... patterns)
+            {
+                return (::match(patterns, value) && ...);
+            },
+            andPat.patterns());
+    }
+};
+
+template <typename Type>
+class Id
+{
+public:
+    Id() = default;
+    template <typename Value>
+    bool match(Value const& value) const
+    {
+        if (mHasValue)
+        {
+            return mValue == value;
+        }
+        mHasValue = true;
+        mValue = value;
+        return true;
+    }
+    Type& value() const
+    {
+        assert(mHasValue);
+        return mValue;
+    }
+private:
+    mutable Type mValue;
+    mutable bool mHasValue{false};
+};
+
+template <typename Type>
+class PatternTraits<Id<Type>>
+{
+public:
+    template <typename Value>
+    static bool match(Id<Type> const& idPat, Value const& value)
+    {
+        return idPat.match(value);
+    }
+};
+
+#endif // _PATTERNS_H_
